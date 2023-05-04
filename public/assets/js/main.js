1160e74686d86ef766672f49b47740a5c2d898c6
Emmanuel Béziat <contact@emmanuelbeziat.com>
2023-05-04 16:32:18 +0200

    views

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-	document.querySelectorAll('select[data-js-choice]')?.forEach(select => {
-		new Choices(select, {
-			searchFields: ['label'],
-			shouldSort: false,
-		})
-	})
+
 })
